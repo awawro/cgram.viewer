1ccd3aafcb08ee80369d1a943371f5304a82f999
Adam Wawro <adamwawro@wp.pl>
2020-08-31 15:29:32 -0700

    added support for SIM and EIC data files (homogenic only)

--- app.R
+++ app.R
@@ -18,7 +18,16 @@ ui <- fluidPage(
             conditionalPanel(condition = "input.side_panel == 'Data'",
                 div(style="margin-bottom:15px"),
                 uiOutput("select_file"),
-                uiOutput("select_mrm"),
+                uiOutput("select_data"),
+                conditionalPanel(condition = "input.data_type == 'MRM'",
+                                 uiOutput("select_mrm")
+                ),
+                conditionalPanel(condition = "input.data_type == 'SIM'",
+                                 uiOutput("select_sim")
+                ),
+                conditionalPanel(condition = "input.data_type == 'EIC'",
+                                 uiOutput("select_eic")
+                ),
                 uiOutput("slider_time_range")
             ),
             # Display panel
@@ -29,7 +38,7 @@ ui <- fluidPage(
                     column(6, sliderInput("legend_cols", "Legend cols", min = 1, max = 5, value = 2, step = 1),),
                     column(6, sliderInput("line_size", "Line (pt)", min = 0.25, max = 1.5 ,value = 0.5, step = 0.25))
                 ),
-                selectInput("facet_by", "Facet by:", choices = c("MRM", "file")),
+                selectInput("facet_by", "Facet by:", choices = c("ion", "file")),
                 fluidRow(
                     column(6, sliderInput("plot_width", "Width (in)", min = 4, max = 12, value = 6, step = 0.25)),
                     column(6, sliderInput("plot_height", "Height (in)", min = 4, max = 12, value = 6, step = 0.25))
@@ -42,7 +51,7 @@ ui <- fluidPage(
                 div(style="margin-bottom:10px"),
                 uiOutput("download_final"),
                 div(style="margin-bottom:10px"),
-                downloadButton("downloadPdfPlot", label = "Download plot as PDF", style = "width:100%;")
+                uiOutput("download_pdf")
             )
             
         ),
@@ -50,8 +59,7 @@ ui <- fluidPage(
         mainPanel(
             tabsetPanel(
                 tabPanel("Plot", 
-                         plotOutput("cgram_plot")),
-                tabPanel("Table", DTOutput("table"))
+                         plotOutput("cgram_plot"))
             )
         )
     )
@@ -65,10 +73,15 @@ server <- function(input, output, session) {
     # initialize reactive values for ggplot storage
     vals <- reactiveValues()
     
-    # use parse.masshunter, add MRM column
+    #output$select_data <- renderUI({
+    #   req(input$datafile)
+    #    selectInput("data_type", "Select data type", choices = c("MRM"))
+    #})
+    
+    # use parse.masshunter
     parsed_datafile <- reactive({
         parse_masshunter_csv(input$datafile$datapath) %>%
-            mutate(MRM = paste(precursor.ion, product.ion, sep = " -> "))
+            {if("MRM" %in% .$type) . <- mutate(., MRM = paste(precursor.ion, product.ion, sep = " -> ")) else .}
     })
     
     output$select_file <- renderUI({
@@ -76,27 +89,53 @@ server <- function(input, output, session) {
         pickerInput("cgram_files_selected", "select cgram files", choices = unique(parsed_datafile()$file), multiple = TRUE)
     })
     
-    output$select_mrm <- renderUI({
+    output$select_data <- renderUI({
         req(input$datafile)
+        selectInput("data_type", "Select data type", choices = unique(parsed_datafile()$type))
+    })
+    
+    output$select_mrm <- renderUI({
+        req(input$datafile, input$data_type == "MRM")
         pickerInput("cgram_mrm_selected", "select MRMs", choices = unique(parsed_datafile()$MRM), multiple = TRUE)
     })
-
+    
+    output$select_sim <- renderUI({
+        req(input$datafile, input$data_type == "SIM")
+        pickerInput("cgram_sim_selected", "select SIMs", choices = unique(parsed_datafile()$SIM), multiple = TRUE)
+    })
+    
+    output$select_eic <- renderUI({
+        req(input$datafile, input$data_type == "EIC")
+        pickerInput("cgram_eic_selected", "select EICs", choices = unique(parsed_datafile()$EIC), multiple = TRUE)
+    })
+    
+    ions_selected <- reactive({
+        case_when(
+            input$data_type == "MRM" ~ list(input$cgram_mrm_selected),
+            input$data_type == "SIM" ~ list(input$cgram_sim_selected),
+            input$data_type == "EIC" ~ list(input$cgram_eic_selected)) %>%
+            unlist()
+    })
+    
     filtered_parsed_datafile <- reactive({
-        filter(parsed_datafile(), file %in% input$cgram_files_selected & MRM %in% input$cgram_mrm_selected)
+        filter(parsed_datafile(),
+                file %in% input$cgram_files_selected &
+                !!sym(input$data_type) %in% ions_selected()
+                )
     })
 
     output$slider_time_range <- renderUI({
-        req(input$datafile, input$cgram_mrm_selected)
+        req(input$datafile, (!is.null(input$cgram_mrm_selected) | !is.null(input$cgram_sim_selected) | !is.null(input$cgram_eic_selected)))
         filtered_time_range <- round(c(min(filtered_parsed_datafile()$time), max(filtered_parsed_datafile()$time)), digits = 1)
         sliderInput("time_range", "select time range", min = filtered_time_range[1], max = filtered_time_range[2], value = c(filtered_time_range[1], filtered_time_range[2]), step = 0.05)
     })
     
     # render plot
     output$cgram_plot <- renderPlot({
-        req(input$datafile, input$cgram_mrm_selected)
+        req(input$datafile, (!is.null(input$cgram_mrm_selected) | !is.null(input$cgram_sim_selected) | !is.null(input$cgram_eic_selected)))
         
         scales_y <- if_else(input$fix_y_axis, "fixed", "free_y")
-        fac_col <- if_else(rep(input$facet_by == "MRM", 2), c("MRM", "file"), c("file", "MRM"))
+        fac_col <- if_else(rep(input$facet_by == "ion", 2), c(input$data_type, "file"), c("file", input$data_type))
         
         gg <- ggplot(data = filtered_parsed_datafile(), aes(x = time, y = intensity, color = !!sym(fac_col[2]))) +
             geom_line(size = input$line_size) +
@@ -111,14 +150,6 @@ server <- function(input, output, session) {
         
         print(gg)
     }, res = 72, width = function() {input$plot_width * 72}, height = function() {input$plot_height * 72})
-    
-    # render table
-    output$table <- renderDT({
-        req(input$datafile)
-        filtered_parsed_datafile() %>%
-            select(file:product.ion) %>%
-            unique()
-    })
 
 }
 
--- downloadData.R
+++ downloadData.R
@@ -7,20 +7,20 @@ output$download_all <- renderUI({
 output$download_full <- downloadHandler(
   filename = "parsed.csv",
   content = function(file) {
-    write.csv(select(parsed_datafile(), -"MRM"), file, row.names = FALSE)
+    write.csv(parsed_datafile(), file, row.names = FALSE)
   }
 )
 
 # Download filtered parsed dataset
 output$download_final <- renderUI({
-  req(input$cgram_mrm_selected)
+  req(input$datafile, (!is.null(input$cgram_mrm_selected) | !is.null(input$cgram_sim_selected) | !is.null(input$cgram_eic_selected)))
   downloadButton("download_filtered", label = "Download filtered dataset", style = "width:100%;")
 })
 
 output$download_filtered <- downloadHandler(
   filename = "filtered.csv",
   content = function(file) {
-    write.csv(select(filtered_parsed_datafile(), -"MRM"), file, row.names = FALSE)
+    write.csv(filtered_parsed_datafile(), file, row.names = FALSE)
   }
 )
 
@@ -31,3 +31,9 @@ output$downloadPdfPlot <- downloadHandler(
     ggsave(file, vals$gg, width = input$plot_width, height = input$plot_height, device = "pdf")
   }
 )
+
+output$download_pdf <- renderUI({
+  req(input$datafile, (!is.null(input$cgram_mrm_selected) | !is.null(input$cgram_sim_selected) | !is.null(input$cgram_eic_selected)))
+  downloadButton("downloadPdfPlot", label = "Download plot as PDF", style = "width:100%;")
+})
+
